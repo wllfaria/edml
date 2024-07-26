98bd6a63139dc5b03c24dd86f483fbbd4d3d3c4b
Willians Faria <dev.willians.faria@gmail.com>
2024-07-25 23:53:58 -0300

    better logger interface

--- logger/logger.ml
+++ logger/logger.ml
@@ -3,7 +3,13 @@ open Effect.Deep
 open Core_unix
 
 type _ t += Log : string -> unit t
-type log_level = Info
+
+type log_level =
+  | Trace
+  | Info
+  | Debug
+  | Warn
+  | Error
 
 let dummy_handler =
   { effc =
@@ -19,7 +25,11 @@ let handler : 'a effect_handler ref = ref dummy_handler
 
 let string_of_log_level level =
   match level with
+  | Trace -> "[TRACE]"
   | Info -> "[INFO]"
+  | Debug -> "[DEBUG]"
+  | Warn -> "[WARN]"
+  | Error -> "[ERROR]"
 ;;
 
 let timestamp () =
@@ -36,11 +46,36 @@ let timestamp () =
 
 type log_mode = Async
 
-let info msg =
-  let msg = string_of_log_level Info ^ " " ^ msg in
+let add_metadata msg level =
+  let msg = string_of_log_level level ^ " " ^ msg in
   let msg = timestamp () ^ " " ^ msg in
   let msg = msg ^ "\n" in
-  perform @@ Log msg
+  msg
+;;
+
+let trace msg =
+  let msg = add_metadata msg Trace in
+  try_with perform (Log msg) !handler
+;;
+
+let info msg =
+  let msg = add_metadata msg Info in
+  try_with perform (Log msg) !handler
+;;
+
+let debug msg =
+  let msg = add_metadata msg Debug in
+  try_with perform (Log msg) !handler
+;;
+
+let warn msg =
+  let msg = add_metadata msg Warn in
+  try_with perform (Log msg) !handler
+;;
+
+let error msg =
+  let msg = add_metadata msg Error in
+  try_with perform (Log msg) !handler
 ;;
 
 let append_to_log_file msg =
@@ -65,7 +100,11 @@ let append_to_log_file msg =
 
 let log msg level =
   match level with
-  | Info -> try_with info msg !handler
+  | Trace -> trace msg
+  | Info -> info msg
+  | Debug -> debug msg
+  | Warn -> warn msg
+  | Error -> error msg
 ;;
 
 let async_handler =
--- logger/logger.mli
+++ logger/logger.mli
@@ -1,5 +1,15 @@
-type log_level = Info
+type log_level =
+  | Trace
+  | Info
+  | Debug
+  | Warn
+  | Error
 type log_mode = Async
 
 val init : string -> log_mode -> unit
 val log : string -> log_level -> unit
+val trace : string -> unit
+val info : string -> unit
+val debug : string -> unit
+val warn : string -> unit
+val error : string -> unit
